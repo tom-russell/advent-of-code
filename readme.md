5555c3e83ef128a6d50f9e7969dfaa793e251b0d
Tom Russell <tom_james_russell@hotmail.com>
2020-12-13 15:12:51 +0000

    add readme and utils

---
+++ readme.md
@@ -0,0 +1,2 @@
+To generate the ASCII Art:
+http://patorjk.com/software/taag/#p=display&f=Big
---
+++ utils.py
@@ -0,0 +1,22 @@
+def load_inputs_as_strings(filename: str) -> [str]:
+    """Read the input file and return into a list of strings."""
+    with open(filename) as f:
+        content = f.read()
+        return content.strip().split('\n')
+
+def load_inputs_as_ints(filename: str) -> [int]:
+    """Read the input file and return into a list of strings."""
+    with open(filename) as f:
+        content = f.read()
+        return [int(x) for x in content.strip().split('\n')]
+        return content.strip().split('\n')
+
+def load_input_as_2d_array(filename: str) -> [int]:
+    """Read the input file and return into a 2D array of strings."""
+    with open(filename) as f:
+        array = []
+        
+        for line in f:
+            array.append(list(line))
+        
+        return array
