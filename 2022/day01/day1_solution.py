850f1ef15820828aa66f6011065a44f38b200376
tom-russell <tom@primer.io>
2022-12-06 00:27:31 +0000

    2022 - day 1

---
+++ 2022/day01/day1_solution.py
@@ -0,0 +1,54 @@
+# --- Day 1 ---
+#    _____      _            _         _____                  _   _             
+#   / ____|    | |          (_)       / ____|                | | (_)            
+#  | |     __ _| | ___  _ __ _  ___  | |     ___  _   _ _ __ | |_ _ _ __   __ _ 
+#  | |    / _` | |/ _ \| '__| |/ _ \ | |    / _ \| | | | '_ \| __| | '_ \ / _` |
+#  | |___| (_| | | (_) | |  | |  __/ | |___| (_) | |_| | | | | |_| | | | | (_| |
+#   \_____\__,_|_|\___/|_|  |_|\___|  \_____\___/ \__,_|_| |_|\__|_|_| |_|\__, |
+#                                                                          __/ |
+#                                                                         |___/ 
+
+import os, sys
+
+
+def load_input(filename):
+    with open(filename) as f:
+        return f.readlines()
+
+
+def part1(input):
+    highest = 0
+    current = 0
+
+    for line in input:
+        try:
+            current += int(line)
+        except ValueError:
+            highest = max(highest, current)
+            current = 0
+
+    print(max(highest, current))
+
+
+def part2(input):
+    values = [0, 0, 0]
+    current = 0
+
+    for line in input:    
+        try:
+            current += int(line)
+        except ValueError:
+            values.append(current)
+            values.sort()
+            values.pop(0)
+            current = 0
+    
+    values.append(current)
+    values.sort()
+    print(sum(values[1:]))
+
+if __name__ == "__main__":
+    input = load_input('day1_input.txt')
+
+    # part1(input)
+    part2(input)
---
+++ 2022/new_day.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+solution_template = """# --- Day {{DAYNO}} ---
+
+import os, sys
+
+
+def load_input(filename):
+    with open(filename) as f:
+        return f.readlines()
+
+
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+
+
+if __name__ == "__main__":
+    input = load_input('day{{DAYNO}}/day{{DAYNO}}_input.txt')
+
+    part1(input)
+    # part2(input)
+"""
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        raise RuntimeError(
+            f"Expected 1 integer argument for the day number. Found {len(sys.argv) - 1}"
+        )
+
+    try:
+        day_no = int(sys.argv[1])
+
+    except ValueError:
+        raise RuntimeError(f"Input could not be converted to integer. Try again.")
+
+    # create the directory for the day's challenge
+    aoc_dir = os.path.dirname(os.path.abspath(__file__))
+    day_dir = os.path.join(aoc_dir, f"day{str(day_no).zfill(2)}")
+    print(day_dir)
+    os.mkdir(day_dir)
+
+    # create the solution file with the template code
+    solution_filename = os.path.join(day_dir, f"day{day_no}_solution.py")
+    with open(solution_filename, "w") as f:
+        f.writelines(solution_template.replace("{{DAYNO}}", str(day_no)))
+
+    # create the empty input files
+    input_filename1 = os.path.join(day_dir, f"day{day_no}_input.txt")
+    input_filename2 = os.path.join(day_dir, f"day{day_no}_input_simple.txt")
+    open(input_filename1, "w")
+    open(input_filename2, "w")
