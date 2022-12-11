09c3625f715e9ed170d528b935f4f8f4cfc77893
tom-russell <tom@primer.io>
2022-12-11 23:12:31 +0000

    02 - Rucksack Reorganisation

---
+++ 2022/day02/day02_solution.py
@@ -0,0 +1,90 @@
+# --- Day day02 ---
+#     ____             __      ____
+#    / __ \____  _____/ /__   / __ \____ _____  ___  _____
+#   / /_/ / __ \/ ___/ //_/  / /_/ / __ `/ __ \/ _ \/ ___/
+#  / _, _/ /_/ / /__/ ,<    / ____/ /_/ / /_/ /  __/ /
+# /_/ |_|\____/\___/_/|_|  /_/    \__,_/ .___/\___/_/
+#                                     /_/
+#    _____      _
+#   / ___/_____(_)_____________  __________
+#   \__ \/ ___/ / ___/ ___/ __ \/ ___/ ___/
+#  ___/ / /__/ (__  |__  ) /_/ / /  (__  )
+# /____/\___/_/____/____/\____/_/  /____/
+#
+#
+import os
+import sys
+
+score_grid = {
+    "A": {
+        "X": 3,
+        "Y": 6,
+        "Z": 0,
+    },
+    "B": {
+        "X": 0,
+        "Y": 3,
+        "Z": 6,
+    },
+    "C": {
+        "X": 6,
+        "Y": 0,
+        "Z": 3,
+    },
+}
+
+
+
+score_grid_2 = {
+    "A": {
+        "X": 0 + 3,
+        "Y": 3 + 1,
+        "Z": 6 + 2,
+    },
+    "B": {
+        "X": 0 + 1,
+        "Y": 3 + 2,
+        "Z": 6 + 3,
+    },
+    "C": {
+        "X": 0 + 2,
+        "Y": 3 + 3,
+        "Z": 6 + 1,
+    },
+}
+
+shape_score = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+
+def load_input(filename):
+    with open(filename) as f:
+        return f.readlines()
+
+
+def part1(input):
+    score = 0
+    for line in input:
+        line = line.strip().split(" ")
+        score += score_grid[line[0]][line[1]]
+        score += shape_score[line[1]]
+
+    print(score)
+
+
+def part2(input):
+    score = 0
+    for line in input:
+        line = line.strip().split(" ")
+        score += score_grid_2[line[0]][line[1]]
+
+    print(score)
+
+
+if __name__ == "__main__":
+    input = load_input("day02/day02_input.txt")
+
+    # part1(input)
+    part2(input)
--- 2022/new_day.py
+++ 2022/new_day.py
@@ -1,8 +1,8 @@
 import os
 import sys
+
 import requests
 from pyfiglet import Figlet
-
 
 solution_template = """# --- Day {{DAYNO}} ---
 # {{ascii}}
@@ -23,7 +23,8 @@ def part2(input):
 
 
 if __name__ == "__main__":
-    input = load_input('day{{DAYNO}}/day{{DAYNO}}_input.txt')
+    input = load_input('{{DAYNO}}/{{DAYNO}}_input_simple.txt')
+    input = load_input('{{DAYNO}}/{{DAYNO}}_input.txt')
 
     part1(input)
     # part2(input)
@@ -38,18 +39,19 @@ if __name__ == "__main__":
 
     try:
         day_no = int(sys.argv[1])
+        day_no = f"day{str(day_no).zfill(2)}"
         day_name = str(sys.argv[2])
     except ValueError:
         raise RuntimeError(f"Input could not be converted to integer. Try again.")
 
     # create the directory for the day's challenge
     aoc_dir = os.path.dirname(os.path.abspath(__file__))
-    day_dir = os.path.join(aoc_dir, f"day{str(day_no).zfill(2)}")
+    day_dir = os.path.join(aoc_dir, day_no)
     print(day_dir)
     os.mkdir(day_dir)
 
     # create the solution file with the template code
-    solution_filename = os.path.join(day_dir, f"day{day_no}_solution.py")
+    solution_filename = os.path.join(day_dir, f"{day_no}_solution.py")
     with open(solution_filename, "w") as f:
         ascii = Figlet("slant").renderText(day_name)
         ascii = ascii.replace("\n", "\n# ")
@@ -57,7 +59,7 @@ if __name__ == "__main__":
         f.writelines(day_output)
 
     # create the empty input files
-    input_filename1 = os.path.join(day_dir, f"day{day_no}_input.txt")
-    input_filename2 = os.path.join(day_dir, f"day{day_no}_input_simple.txt")
+    input_filename1 = os.path.join(day_dir, f"{day_no}_input.txt")
+    input_filename2 = os.path.join(day_dir, f"{day_no}_input_simple.txt")
     open(input_filename1, "w")
     open(input_filename2, "w")
